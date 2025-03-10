9fa0badaa370afcddc819929bce3c5cdfdb1681d
Kirucoderanger <klemu@byupathway.edu>
2025-03-10 15:59:43 +0300

    portfolio

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import '../assets/styles/Footer.scss'
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <a href="https://github.com/Kirucoderanger/wdd131" target="_blank" rel="noreferrer noopener" title="GitHub Profile"><GitHubIcon/></a>
+        <a href="www.linkedin.com/in/kirubel-mekonen-251296b5" target="_blank" rel="noreferrer" title="LinkedIn Profile"><LinkedInIcon/></a>
+      </div>
+      <p>A portfolio designed & built by <a href="https://kirucoderanger.github.io/wdd131/" target="_blank" rel="noreferrer noopener">Kirubel Mekonen</a> with </p>
+    </footer>
+  );
+}
+
+export default Footer;
