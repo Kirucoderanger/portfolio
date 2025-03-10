0b2d77de9d2a7300a53aaf00dbbc55660441aa01
Kirucoderanger <klemu@byupathway.edu>
2025-03-10 19:31:32 +0300

    icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,10 +7,10 @@ function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a href="https://github.com/Kirucoderanger" target="_blank" rel="noopener noreferrer" title="GitHub Profile"><GitHubIcon/></a>
+        <a href="https://www.linkedin.com/in/kirubel-mekonen-251296b5/" target="_blank" rel="noopener noreferrer" title="LinkedIn Profile"><LinkedInIcon/></a>
       </div>
-      <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a> with 💜</p>
+      <p>A portfolio designed & built by <a href="https://kirucoderanger.github.io/portfolio/" target="_blank" rel="noopener noreferrer">Kirubel Mekonen</a> with 💜</p>
     </footer>
   );
 }
